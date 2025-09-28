2ec69ef30a2caca21c6b289d833b6f04e3292356
soham518 <167617833+soham518@users.noreply.github.com>
2025-09-28 20:05:43 +0530

    Refactor transaction example to improve readability and log sender/receiver balances

--- examples/2_send_signed_transaction.js
+++ examples/2_send_signed_transaction.js
@@ -1,4 +1,6 @@
-require("dotenv").config({ path: require('path').join(__dirname, '..', '.env') });
+require("dotenv").config({
+  path: require("path").join(__dirname, "..", ".env"),
+});
 const { ethers } = require("ethers");
 
 // Import private key helper
@@ -7,19 +9,50 @@ const url = `https://virtual.mainnet.eu.rpc.tenderly.co/${process.env.TENDERLY_R
 
 const provider = new ethers.JsonRpcProvider(url);
 // Setup connection
-
+const RECEIVER_ADDRESS = "0x5098328000388440c397E6d8649339096f90d63f";
 async function main() {
   const private_key = await promptForKey();
   console.log(private_key);
   //setting up the wallet
   const wallet = new ethers.Wallet(private_key, provider);
-  //getting wallet balance
-  const balance = await provider.getBalance(wallet.address);
+  //getting wallet balance from sender and receiver
+  const sendersBalanceBefore = await provider.getBalance(wallet.address);
+  const receiversBalanceBefore = await provider.getBalance(RECEIVER_ADDRESS);
   //loging the balance
-  console.log(`Balance: ${ethers.formatEther(balance)} ETH`);
+  console.log(
+    `Senders Balance Befor Transaction: ${ethers.formatEther(
+      sendersBalanceBefore
+    )} ETH\n`
+  );
+  console.log(
+    `Receivers Balance Befor Transaction: ${ethers.formatEther(
+      receiversBalanceBefore
+    )} ETH\n`
+  );
 
+  const transaction = await wallet.sendTransaction({
+    to: RECEIVER_ADDRESS,
+    value: ethers.parseUnits("1", 18),
+  });
 
+  const recept = await transaction.wait();
+
+  const sendersBalanceAfter = await provider.getBalance(wallet.address);
+  const receiversBalanceAfter = await provider.getBalance(RECEIVER_ADDRESS);
+
+  console.log(transaction);
+  console.log(recept);
+  console.log(
+    `Senders Balance After Transaction: ${ethers.formatEther(
+      sendersBalanceAfter
+    )} ETH\n`
+  );
+  console.log(
+    `Receivers Balance After Transaction: ${ethers.formatEther(
+      receiversBalanceAfter
+    )} ETH\n`
+  );
 }
 
 main();
-//6755f93f41a954c2052ad032aa3d13cf1c7aeaa2e17cbdc7f34990d6ec3dde47
+
