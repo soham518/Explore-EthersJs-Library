9b56760ff4a070420cca71f5b13adf6b3fbfe404
soham518 <167617833+soham518@users.noreply.github.com>
2025-09-28 22:01:28 +0530

    Fix contract ABI definition and ensure proper instantiation of the contract

--- examples/3_read_smart_contracts.js
+++ examples/3_read_smart_contracts.js
@@ -9,11 +9,11 @@ const url = `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}
 //provider is required to read data by interacting with ethereum blockchain
 const provider = new ethers.JsonRpcProvider(url);
 const ERC20_ADDRESS = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984";
-const ERC20_ABI = ["function new() view returns (string)"];
+const ERC20_ABI = ["function name() view returns (string)"];
 
 //abi - application binary interface is basically a part of smartcontract which is used to interact with the function present in the smart contract;
 
-const contract = ethers.Contract(ERC20_ADDRESS, ERC20_ABI, provider);
+const contract = new ethers.Contract(ERC20_ADDRESS, ERC20_ABI, provider);
 //provider to read data from the contract and signer to write data through contract.
 
 async function main(params) {
