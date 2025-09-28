c456445b29b45f566086947e95315e378a18cebe
soham518 <167617833+soham518@users.noreply.github.com>
2025-09-28 18:00:10 +0530

    learnt basics of ethers js

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -1,18 +1,17 @@
-require("dotenv").config()
-const { ethers } = require("ethers")
-
+const { ethers } = require("ethers");
+require("dotenv").config();
 // Setup connection
-const URL = `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
-const provider = new ethers.JsonRpcProvider(URL)
+const url = `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHIMY_API_KEY}`;
+const provider = new ethers.JsonRpcProvider(url);
 
-const ADDRESS = "0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e"
-
+const ADDRESS = "0x4838B106FCe9647Bdf1E7877BF73cE8B0BAD5f97";
 async function main() {
   // Get balance
-  const balance = await provider.getBalance(ADDRESS)
-
-  // Log balance
-  console.log(`\nETH Balance of ${ADDRESS} --> ${ethers.formatUnits(balance, 18)} ETH\n`)
+  //provider is to read any data from the blockchain.
+  //signer is for writing any data on the blockchain
+  const balance = await provider.getBalance(ADDRESS);
+  // format ether is used to convert the balance from wei to eth.
+  console.log(`Balance: ${ethers.formatEther(balance)} ETH`);
 }
 
-main()
+main();
