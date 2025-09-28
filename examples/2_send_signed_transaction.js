0e3522525848d4fea273359211cfe0729c97a96c
soham518 <167617833+soham518@users.noreply.github.com>
2025-09-28 19:20:05 +0530

    Update dotenv configuration to specify path for environment variables

--- examples/2_send_signed_transaction.js
+++ examples/2_send_signed_transaction.js
@@ -1,13 +1,25 @@
-require("dotenv").config();
+require("dotenv").config({ path: require('path').join(__dirname, '..', '.env') });
 const { ethers } = require("ethers");
 
 // Import private key helper
 const { promptForKey } = require("../helpers/prompt.js");
-const url = process.env.TENDERLY_RPC_URL;
+const url = `https://virtual.mainnet.eu.rpc.tenderly.co/${process.env.TENDERLY_RPC_URL}`;
+
+const provider = new ethers.JsonRpcProvider(url);
 // Setup connection
 
 async function main() {
   const private_key = await promptForKey();
+  console.log(private_key);
+  //setting up the wallet
+  const wallet = new ethers.Wallet(private_key, provider);
+  //getting wallet balance
+  const balance = await provider.getBalance(wallet.address);
+  //loging the balance
+  console.log(`Balance: ${ethers.formatEther(balance)} ETH`);
+
+
 }
 
 main();
+//6755f93f41a954c2052ad032aa3d13cf1c7aeaa2e17cbdc7f34990d6ec3dde47
